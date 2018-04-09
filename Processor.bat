fff0505bb016c41bb6ae47e2248d29951883cee2
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2018-04-09 11:58:41 +0200

    helper bat files for console startup

---
+++ Processor.bat
@@ -0,0 +1,6 @@
+set BASE_URL=http://localhost:8080/FROST-Server/v1.0/
+set BROKER=10.1.9.185
+set SESSION=0815
+set WORKERS=10
+set POSTDELAY=1
+java -cp .\target\frostBenchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar frostBenchmark.Processor
