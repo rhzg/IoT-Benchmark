7213af83163b5758482d23854dd926a28ea67ea9
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2018-04-10 12:28:03 +0200

    increase default worker

--- SensorCluster.bat
+++ SensorCluster.bat
@@ -3,7 +3,7 @@ set BROKER=10.1.9.185
 # Benchmark Session Identifier
 set SESSION=0815
 # number of parallel Sensors data providers
-set WORKERS=10   
+set WORKERS=100
 # msec delay between observations
 set POSTDELAY=1
 java -cp .\target\frostBenchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar frostBenchmark.SensorCluster
