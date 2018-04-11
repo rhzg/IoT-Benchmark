4fd1dc2d408a61de83a1ca2b936d4d9161821fa3
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2018-04-11 11:45:08 +0200

    define all environment variables in one single bat file

--- Processor.bat
+++ Processor.bat
@@ -1,8 +1,1 @@
-set BASE_URL=http://localhost:8080/FROST-Server/v1.0/
-# Broker for session updates 
-set BROKER=10.1.9.185
-# Benchmark Session Identifier
-set SESSION=0815
-# Percentage of Datastreams covered
-set COVERAGE=50
 java -cp .\target\frostBenchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar frostBenchmark.Processor
--- SensorCluster.bat
+++ SensorCluster.bat
@@ -1,9 +1,1 @@
-set BASE_URL=http://localhost:8080/FROST-Server/v1.0/
-set BROKER=10.1.9.185
-# Benchmark Session Identifier
-set SESSION=0815
-# number of parallel Sensors data providers
-set WORKERS=100
-# msec delay between observations
-set POSTDELAY=1
 java -cp .\target\frostBenchmark-0.0.1-SNAPSHOT-jar-with-dependencies.jar frostBenchmark.SensorCluster
