c481e72aa74ad8413b677446df9d344542a44d8e
Reinhard Herzog <Reinhard.Herzog@iosb.fraunhofer.de>
2018-05-16 18:03:00 +0200

    add property

--- BenchmarkData/src/main/java/frostBenchmark/BenchProperties.java
+++ BenchmarkData/src/main/java/frostBenchmark/BenchProperties.java
@@ -13,6 +13,10 @@ public class BenchProperties {
 	static final String PROXYHOST = "proxyhost";
 	static final String PROXYPORT = "proxyport";
 
+	static final String TIMEOUT = "timeout";
+	static final String TIMEOUTdefault = "10000";
+	static       int    timeout = 10000;
+
 	static final String POSTDELAY = "POSTDELAY";
 	static final String POSTDELAYdefault = "1000";
 	static       int    postdelay = 1000;
@@ -21,23 +25,27 @@ public class BenchProperties {
 	static final String COVERAGEdefault = "100";
 	static       int    coverage = 100;
 
+	static final String PERIOD = "PERIOD";
+	static final String PERIODdefault = "10";
+	static       int    period = 100;
+
 	static final String WORKERS = "WORKERS";
 	static final String WORKERSdefault = "1";
 	static       int    workers = 1;
-
-	static final String BROKER = "BROKER";
-	static final String BROKERdefault = "localhost";
 
 	static final String STATUS = "status";
 	static final String RUNNING = "running";
 	static final String FINISHED = "finished";
 	static final String TERMINATE = "terminate";
 
-//	static private JSONObject properties = null;
 	static private Map<String, Object> properties = null;
 
 	public static Map<String, Object> getProperties() {
 		return properties;
+	}
+	
+	public static void setProperties(Map<String, Object> properties) {
+		BenchProperties.properties = properties;
 	}
 
 	static public void intialize() {
@@ -50,22 +58,29 @@ public class BenchProperties {
 			properties.put(WORKERS, workersStr);
 			workers = Integer.parseInt(workersStr);
 
-			String broker = System.getenv(BROKER);
-			if (broker == null)
-				broker = BROKERdefault;
-			properties.put(BROKER, broker);
-
 			String coverageStr = System.getenv(COVERAGE);
 			if (coverageStr == null)
 				coverageStr = COVERAGEdefault;
 			properties.put(COVERAGE, coverageStr);
 			coverage = Integer.parseInt(coverageStr);
 
+			String rateStr = System.getenv(PERIOD);
+			if (rateStr == null)
+				rateStr = PERIODdefault;
+			properties.put(PERIOD, rateStr);
+			period = Integer.parseInt(rateStr);
+
 			String postdelayStr = System.getenv(POSTDELAY);
 			if (postdelayStr == null)
 				postdelayStr = POSTDELAYdefault;
-			properties.put(COVERAGE, postdelayStr);
+			properties.put(POSTDELAY, postdelayStr);
 			postdelay = Integer.parseInt(postdelayStr);
+
+			String timeoutStr = System.getenv(TIMEOUT);
+			if (timeoutStr == null)
+				timeoutStr = TIMEOUTdefault;
+			properties.put(TIMEOUT, timeoutStr);
+			timeout = Integer.parseInt(timeoutStr);
 
 			properties.put(STATUS, FINISHED);
 		}
