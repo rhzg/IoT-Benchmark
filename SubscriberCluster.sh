3cc5c900a637db952b994d8a0506b71e19e00cc2
Hylke van der Schaaf <hylke.vanderschaaf@iosb.fraunhofer.de>
2018-05-18 11:09:13 +0200

    Added unix shell scripts.

---
+++ SubscriberCluster.sh
@@ -0,0 +1,15 @@
+# URL to be used for creating, subscribing and reading data
+export BASE_URL=http://localhost:8080/FROST-Server/v1.0/
+
+# mqtt broker address
+# set BROKER=192.168.99.100
+export BROKER=localhost
+
+# Benchmark Session Identifier within Benchmark thing to be used
+export SESSION=0815
+
+# Percentage of Datastreams covered by mqtt subsribers
+export COVERAGE=50
+
+
+java -jar ./SubscriberCluster/target/SubscriberCluster-0.0.1-SNAPSHOT-jar-with-dependencies.jar
